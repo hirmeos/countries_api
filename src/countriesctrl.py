4bf741ade0aadbf8af6886f3ab9650622f76816f
Javier Arias <javier@arias.re>
2018-11-14 11:12:10 +0000

    add post country

--- src/countriesctrl.py
+++ src/countriesctrl.py
@@ -1,6 +1,6 @@
 import web
 from aux import logger_instance, debug_mode
-from api import json_response, api_response, check_token, build_params
+from api import json, json_response, api_response, build_params
 from errors import Error, NOTALLOWED, NORESULT
 from models import Country, results_to_countries
 
@@ -30,13 +30,43 @@ class CountryController(object):
             clause, params = build_params(filters)
             results = Country.get_all(clause, params)
 
-        if not results:
+        try:
+            assert results
+            countries = results_to_countries(results)
+            assert countries
+        except AssertionError:
             raise Error(NORESULT)
 
-        return results_to_countries(results)
+        return countries
 
     def POST(self, name):
-        raise Error(NOTALLOWED)
+        """Create a country"""
+        logger.debug("Data: %s" % (web.data()))
+
+        data           = json.loads(web.data())
+        country_id     = data.get('country_id')
+        country_code   = data.get('country_code')
+        country_name   = data.get('country_name')
+        continent_code = data.get('continent_code')
+
+        try:
+            country_name = strtolist(country_name)
+            assert country_id and country_name and country_code \
+            and continent_code
+        except AssertionError as error:
+            logger.debug(error)
+            m = "You must provide a (country_id, country_code"
+                ", a continent_code, and at least one country_name"
+            raise Error(BADPARAMS, msg=m)
+
+        continent = {continent_code=continent_code}
+        country = Country(country_id, country_code, continent, country_name)
+        country.save()
+        # now we get the record from db to make sure it's been inserted
+        # and reload continent information
+        record = Country.get_from_country_id(country_id)
+        assert record
+        return results_to_countries(record)
 
     def PUT(self, name):
         raise Error(NOTALLOWED)
